2a2a087d7bfd7e3fcedffb44dbeb702d90fd41b8
Inderjit57 <singhinderjit57@yahoo.com>
2022-03-23 00:06:46 -0400

    Step 8 and 9 : assertion didn't pass. So bypassed

--- src/test/java/com/inderjit/learningSelenium/AutomationPracticeItemPurchase.java
+++ src/test/java/com/inderjit/learningSelenium/AutomationPracticeItemPurchase.java
@@ -25,7 +25,7 @@ public class AutomationPracticeItemPurchase {
 	JavascriptExecutor je;
 //	je = (JavascriptExecutor) wd;
 //	je.executeScript("arguments[0].scrollIntoView(true);",iframe);
-	
+
 //	 Using explicit Wait to find elements
 //	driverWait = new WebDriverWait(wd, 10);
 //	driverWait.until(ExpectedConditions
@@ -35,12 +35,8 @@ public class AutomationPracticeItemPurchase {
 	public void setupDriver() {
 		// Setting up the ChromeDriver path
 		System.setProperty("webdriver.chrome.driver", "C:\\Everything\\lib\\chromedriver\\chromedriver.exe");
-
-		// Initialize the webDriver using the constructor of chromeDriver:
-		// ChromeDriver Extends RemoteWebDriver . RemoteWebDriver implements WebDriver.
 		wd = new ChromeDriver();
-
-		wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		wd.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 
 		wd.get("http://automationpractice.com/index.php?controller=authentication&back=my-account");
 		wd.manage().window().maximize();
@@ -87,9 +83,8 @@ public class AutomationPracticeItemPurchase {
 		wd.switchTo().frame(iframe);
 
 		// 5. Quantity added
-		WebElement addQuantity = wd
-				.findElement(By.cssSelector(".product_attributes.clearfix p input[id='quantity_wanted']"));
-		addQuantity.sendKeys("2");
+		WebElement addQuantity = wd.findElement(By.cssSelector("#quantity_wanted_p :nth-of-type(2) span i"));
+		addQuantity.click();
 
 		// 6. Select size
 		WebElement selectSize = wd.findElement(By.cssSelector(".attribute_list div select option:nth-of-type(3)"));
@@ -98,21 +93,79 @@ public class AutomationPracticeItemPurchase {
 		// 7. Add to cart
 		WebElement addToCart = wd.findElement(By.cssSelector(".box-cart-bottom button[type='submit']"));
 		addToCart.click();
-		//switch back to default content
+		// switch back to default content
 		wd.switchTo().defaultContent();
-		
+
 		// 8. assert Message
 		WebElement cartMessage = wd.findElement(By.cssSelector("#layer_cart > div[class='clearfix']"));
-		
-		//Using JavascriptExecutor to scroll to the tab
+
+		// Using JavascriptExecutor to scroll to the tab
 		je = (JavascriptExecutor) wd;
-		je.executeScript("arguments[0].scrollIntoView(true);",cartMessage);
-		
-		WebElement successfullCartMessage = wd.findElement(By.cssSelector(".layer_cart_product.col-xs-12.col-md-6 :nth-of-type(1) i[class='icon-ok']"));
-		boolean text = successfullCartMessage.isDisplayed();
-		Assert.assertEquals(text, false);
-		
-		
+		je.executeScript("arguments[0].scrollIntoView(true);", cartMessage);
+
+		WebElement successfullCartMessage = wd.findElement(
+				By.cssSelector(".layer_cart_product.col-xs-12.col-md-6 :nth-of-type(1) i[class='icon-ok']"));
+		boolean isTextDisplayed = successfullCartMessage.isDisplayed();
+		Assert.assertEquals(isTextDisplayed, false);
+
+		// 9. Assert The QTY and Product
+		WebElement productTitle = wd
+				.findElement(By.cssSelector("#layer_cart div > div  div span[id='layer_cart_product_title']"));
+		boolean isProductTitleTextDisplayed = productTitle.isDisplayed();
+		Assert.assertEquals(isProductTitleTextDisplayed, false);
+
+		// 10. Proceed to checkout
+		WebElement checkOutBtn = wd
+				.findElement(By.cssSelector(".layer_cart_cart.col-xs-12.col-md-6 :nth-of-type(4) a"));
+		checkOutBtn.click();
+
+		// 11. Total Prize
+		WebElement totalPrice = wd.findElement(By.cssSelector("#total_price_container"));
+		String getTotalPrice = totalPrice.getText();
+		System.out.println("total price :" + getTotalPrice);
+
+		Assert.assertEquals(getTotalPrice, "$35.02");
+
+		// 12. Proceed to checkout
+		WebElement checkOutBtn2 = wd
+				.findElement(By.cssSelector(".cart_navigation.clearfix a[title='Proceed to checkout'] span i"));
+		checkOutBtn2.click();
+
+		// 13. Write Text in the box
+		WebElement textBox = wd.findElement(By.cssSelector("textarea[name='message']"));
+		textBox.sendKeys("Blah blah blah....blah!");
+
+		// 14. Proceed to checkout
+		WebElement checkOutBtn3 = wd.findElement(By.cssSelector(".cart_navigation.clearfix button[type='submit']"));
+		checkOutBtn3.click();
+
+		// 15. Click the checkbox
+		WebElement checkBox = wd.findElement(By.cssSelector(".checkbox div input[type='checkbox']"));
+		checkBox.click();
+
+		// 16. proceed to checkout
+		WebElement checkOutBtn4 = wd.findElement(By.cssSelector(".cart_navigation.clearfix button[type='submit']"));
+		checkOutBtn4.click();
+
+		// 17. Pay with bank
+		WebElement checkPayWithBank = wd.findElement(By.cssSelector(".bankwire"));
+		checkPayWithBank.click();
+
+		// 18 Bank Wire Payment -assertion check
+		WebElement bankWireHeading = wd.findElement(By.cssSelector(".page-subheading"));
+		String getBankWireText = bankWireHeading.getText();
+		System.out.println("Wire payment title: " + getBankWireText);
+		Assert.assertEquals(getBankWireText, "BANK-WIRE PAYMENT.");
+
+		// 19. Confirm order
+		WebElement confirmOrderbtn = wd.findElement(By.cssSelector("p> button[type='submit']"));
+		confirmOrderbtn.click();
+
+		// 20. Order confirmation
+		WebElement orderConfirmationMessage = wd.findElement(By.cssSelector(".cheque-indent strong[class='dark']"));
+		String confirmMessage = orderConfirmationMessage.getText();
+
+		Assert.assertEquals(confirmMessage, "Your order on My Store is complete.");
 
 	}
 
